ece70280a15d0ab8ea2498b9c88e229a4dbf09a2
Opisek <darowski.kacper@gmail.com>
2024-02-27 13:37:41 +0100

    dockerized

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# build
+FROM node:18-alpine as build
+
+WORKDIR /app
+
+COPY package.json .
+RUN npm i --production
+RUN cp -r node_modules /tmp/node_modules_prod
+RUN npm i
+
+COPY src src
+COPY static static
+COPY svelte.config.js .
+COPY tsconfig.json .
+COPY vite.config.ts .
+RUN ls
+RUN npm run build
+
+# runtime
+FROM node:18-alpine as runtime
+
+WORKDIR /app
+
+COPY --from=build /tmp/node_modules_prod ./node_modules
+COPY --from=build /app/build ./build
+COPY package.json .
+
+USER node:node
+CMD [ "node", "build/index.js" ]
--- src/routes/[...redirect]/+page.server.ts
+++ src/routes/[...redirect]/+page.server.ts
@@ -1,10 +1,18 @@
 import { error, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import redirects from "../../config/redirects.json";
 
-export const load: PageServerLoad = ({ params }) => {
+import fs from "node:fs/promises";
+
+export const load: PageServerLoad = async ({ params }) => {
 	const parts = params.redirect.split("/");
 	const firstPart = parts.shift();
+
+	let redirects;
+	try {
+		redirects = await fs.readFile("/app/config/redirects.json").then(content => JSON.parse(content));
+	} catch (_) {
+		error(404, 'Not found');
+	}
 
 	for (const entry of redirects)
 		for (const match of entry.pages)
