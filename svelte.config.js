347246cc2213307eb7e0a51c26bb8602031be7a8
Opisek <darowski.kacper@gmail.com>
2024-03-01 15:26:41 +0100

    reverted js to ts switch

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
-const config: import('@sveltejs/kit').Config = {
+/** @type {import('sveltejs/kit').Config} */
+const config = {
 	preprocess: preprocess({}),
 	kit: {
 		adapter: adapter()
