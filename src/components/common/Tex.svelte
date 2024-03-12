2f362168b956ff863660c27e23adb9833e91490b
Opisek <darowski.kacper@gmail.com>
2024-03-12 22:06:31 +0100

    decrease tex size

--- src/components/common/Tex.svelte
+++ src/components/common/Tex.svelte
@@ -9,7 +9,16 @@
 	}
 	
 	$: katexString = katex.renderToString(math, options);
+
 </script>
+
+<style lang="scss">
+	@import "../../styles/dimensions.scss";
+
+	:global(.katex) {
+		font-size: $fontSize;
+	}
+</style>
 
 <svelte:head>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous">
