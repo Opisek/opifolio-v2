db4d906654728d22bd77689090911cf46ca855c2
Opisek <darowski.kacper@gmail.com>
2024-03-14 13:07:25 +0100

    change amount to limit in api

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,7 +1,7 @@
 import { getPosts, DEFAULT_LIMIT } from '$lib/server/db.js';
 
 export function GET({ url }) {
-  const amount = Number.parseInt(url.searchParams.get('amount') || DEFAULT_LIMIT.toString());
+  const amount = Number.parseInt(url.searchParams.get('limit') || DEFAULT_LIMIT.toString());
   const offset = Number.parseInt(url.searchParams.get('offset') || "0");
 
   const posts = getPosts(amount, offset);
--- src/routes/api/search/+server.ts
+++ src/routes/api/search/+server.ts
@@ -3,7 +3,7 @@ import { searchPosts, DEFAULT_LIMIT } from '$lib/server/db.js';
 export function GET({ url }) {
   const query = url.searchParams.get('query');
   const tag = url.searchParams.get('tag');
-  const amount = Number.parseInt(url.searchParams.get('amount') || DEFAULT_LIMIT.toString());
+  const amount = Number.parseInt(url.searchParams.get('limit') || DEFAULT_LIMIT.toString());
   const offset = Number.parseInt(url.searchParams.get('offset') || "0");
 
   const result = searchPosts(query, tag, amount, offset);
