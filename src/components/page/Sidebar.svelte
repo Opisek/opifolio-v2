838613317a076731d515494c9ecafea36bec98b3
Opisek <darowski.kacper@gmail.com>
2024-03-16 00:35:30 +0100

    remove sidebar scrollbar due to overflow-x issues

--- src/components/page/Sidebar.svelte
+++ src/components/page/Sidebar.svelte
@@ -8,11 +8,11 @@
     position: sticky;
     top: $navbarHeight + $paddingSmall;
     max-width: 20vw;
-    min-width: fit-content;
+    //min-width: fit-content;
 
-    max-height: calc(100vh - $navbarHeight - $paddingSmall);
-    overflow-y: auto;
-    overflow-x: hidden;
+    //max-height: calc(100vh - $navbarHeight - $paddingSmall);
+    //overflow-y: auto;
+    //overflow-x: visible !important;
 
     display: flex;
     flex-direction: column;
