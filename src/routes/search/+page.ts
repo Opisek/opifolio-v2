8c6f934a5160e5b6b40c11a6653240031267158a
Opisek <darowski.kacper@gmail.com>
2024-03-22 20:56:29 +0100

    fix empty search results

--- src/routes/search/+page.ts
+++ src/routes/search/+page.ts
@@ -4,13 +4,15 @@ import { browser } from '$app/environment';
 import { goto } from '$app/navigation';
 
 export const load: PageLoad = async ({ fetch, url }) => {
+  const currentPage = Number.parseInt(url.searchParams.get("page") || "1");
+
   const [ posts, tags, count ] = await Promise.all([
     fetch(`/api/search${url.search}`).then(res => res.json()),
     fetch("/api/tags").then(res => res.json()), // TODO: caching
     fetch(`/api/search/pageCount${url.search}`).then(res => res.json()) // TODO: caching
   ]);
 
-  if (posts.length == 0) {
+  if (posts.length == 0 && currentPage > 1) {
     url.searchParams.set("page", count.toString());
     if (browser) goto(url.toString(), { replaceState: true });
     else throw redirect(302, url.toString());
