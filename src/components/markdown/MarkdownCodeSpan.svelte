d2d93bcb1fdbf960ac845b4cb7863af113399d75
Opisek <darowski.kacper@gmail.com>
2024-04-03 11:04:21 +0200

    remove evil codespan whitespace

--- src/components/markdown/MarkdownCodeSpan.svelte
+++ src/components/markdown/MarkdownCodeSpan.svelte
@@ -24,9 +24,9 @@
 </style>
 
 <code>
-  {@html text.replaceAll("&amp;", "&")}
-</code>
+  {@html text.replaceAll("&amp;", "&")}<!--
+--></code><!--
 
-<svelte:head>
+--><svelte:head>
   <link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap" rel="stylesheet">
 </svelte:head>
